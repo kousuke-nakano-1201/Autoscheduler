9fd7e5d4f0e3757f89147b6be346fcd9106010ae
kousuke.nakano <ikikewawimps1201@ezweb.ne.jp>
2018-11-05 10:35:20 +0000

    karikansei

--- app/controllers/tables_controller.rb
+++ app/controllers/tables_controller.rb
@@ -1,6 +1,16 @@
 class TablesController < ApplicationController
+  before_action :require_user_logged_in
+  before_action :correct_user
+  
   def index
     @schedule = Schedule.find(params[:schedule_id])
     @tasks = @schedule.tasks
+  end
+  
+  def correct_user
+    @schedule = current_user.schedules.find_by(id: params[:schedule_id])
+    unless @schedule
+      redirect_to root_url
+    end
   end
 end
